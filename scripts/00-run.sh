037760e391f1771ae8bb05c1abc41aa6a790bba1
Aman <aman.mukhtar@coeus-solutions.de>
2021-11-19 15:10:21 +0500

    IT-15031 : Moving composer install back to build

--- scripts/00-run.sh
+++ scripts/00-run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail
 
-composer global require "hirak/prestissimo:^0.3"
-composer install
-
 # Running Migrations
 echo "Starting migrations! ..."
 php artisan migrate --force
