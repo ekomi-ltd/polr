23329f4e42dfda4b6967fa72a4ebbb789049008c
Aman <aman.mukhtar@coeus-solutions.de>
2021-11-23 17:45:42 +0500

    IT-15031 : Checking env vars

--- app/Http/Controllers/StaticPageController.php
+++ app/Http/Controllers/StaticPageController.php
@@ -5,12 +5,10 @@ use Illuminate\Http\Request;
 class StaticPageController extends Controller {
     /**
      * Show static pages such as the about page.
-     *
-     * @return Response
      */
     public function displayAbout(Request $request) {
         echo "in about page polr";
-        $user_role = session('role');
+       // $user_role = session('role');
        // return view('about', ['role' => $user_role, 'no_div_padding' => true]);
     }
 }
